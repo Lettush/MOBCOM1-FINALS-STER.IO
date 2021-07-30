b8de063f48e77398de7b9ce66b3298e653f1cca4
Lettush <jaredneilhortaleza11@gmail.com>
2021-07-30 22:07:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sterio/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.example.sterio
+
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+    private var totalTime: Int = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mp = MediaPlayer.create(this, R.raw.music)
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
+        totalTime = mp.duration
+
+        // Volume Bar
+        val volumeBar = findViewById<SeekBar>(R.id.volumeBar)
+        volumeBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        var volumeNum = progress / 100.0f
+                        mp.setVolume(volumeNum, volumeNum)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+        )
+
+        // Position Bar
+        val positionBar = findViewById<SeekBar>(R.id.positionBar)
+        positionBar.max = totalTime
+        positionBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        mp.seekTo(progress)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+        )
+
+        // Thread
+        Thread(Runnable {
+            while (mp != null) {
+                try {
+                    var msg = Message()
+                    msg.what = mp.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+
+                }
+            }
+        }).start()
+    }
+
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            val positionBar = findViewById<SeekBar>(R.id.positionBar)
+            val elapsedTimeLabel = findViewById<TextView>(R.id.elapsedTimeLabel)
+            val remainingTimeLabel = findViewById<TextView>(R.id.remainingTimeLabel)
+
+            var currentPosition = msg.what
+
+            // Update positionBar
+            positionBar.progress = currentPosition
+
+            // Update Labels
+            var elapsedTime = createTimeLabel(currentPosition)
+            elapsedTimeLabel.text = elapsedTime
+
+            var remainingTime = createTimeLabel(totalTime - currentPosition)
+            remainingTimeLabel.text = "-$remainingTime"
+        }
+    }
+
+    fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        var min = time / 1000 / 60
+        var sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
+    }
+
+    fun playBtnClick(v: View) {
+        val playBtn = findViewById<Button>(R.id.playBtn)
+        if (mp.isPlaying) {
+            // Stop
+            mp.pause()
+            playBtn.setBackgroundResource(R.drawable.play)
+        } else {
+            // Start
+            mp.start()
+            playBtn.setBackgroundResource(R.drawable.stop)
+        }
+
+    }
+}
